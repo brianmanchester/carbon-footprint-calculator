23da7845961315e216a2e68f16d0d414dd09e673
Brian Manchester <brian.l.manchester@gmail.com>
2022-09-30 20:27:27 -0600

    Add Chakra provider and layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Layout } from '@/components/layout';
 
 function EmissionsCalculator({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -14,10 +16,14 @@ function EmissionsCalculator({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       {/* @ts-ignore: Issue with latest version of Next.js. See https://github.com/vercel/next.js/issues/40372 */}
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
       </Hydrate>
     </QueryClientProvider>
   );
 }
 
-export default EmissionsCalculator
+export default EmissionsCalculator;
