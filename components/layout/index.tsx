a1538f6dfd9d6f181e6d2b06da8beee3b2b6ab8e
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-01 18:48:16 -0600

    Make logo link to home and change styles

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -10,6 +10,7 @@ import {
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import styles from '@/styles/Layout.module.css';
 import { useNotifications } from 'contexts/notifications';
+import NextLink from 'next/link';
 
 export type LayoutProps = {
   children: React.ReactNode;
@@ -24,38 +25,54 @@ export const Layout = ({ children }: LayoutProps) => {
 
   return (
     <>
-      <div className={styles.topbar}>
-        <div className={styles['logo-slogan']}>
-          <Image
-            src='/logo.png'
-            alt='Emissions calculator logo'
-            width={32}
-            height={32}
-          />
-          <span>
-            <Highlight
-              styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
-              query='Your'
-            >
-              Reduce Your Carbon Footprint
-            </Highlight>
-          </span>
+      <div className={styles['topbar-container']}>
+        <div className={styles.topbar}>
+          <NextLink href='/'>
+            <a>
+              <div className={styles['logo-slogan']}>
+                <Image
+                  src='/logo.png'
+                  alt='Emissions calculator logo'
+                  width={32}
+                  height={32}
+                />
+                <span>
+                  <Highlight
+                    styles={{
+                      px: '2',
+                      py: '1',
+                      rounded: 'full',
+                      bg: 'teal.100'
+                    }}
+                    query='Your'
+                  >
+                    Reduce Your Carbon Footprint
+                  </Highlight>
+                </span>
+              </div>
+            </a>
+          </NextLink>
+          <Link
+            className={styles['code-link']}
+            href='https://github.com/brianmanchester/carbon-footprint-calculator'
+            isExternal
+            color='teal.500'
+          >
+            <span>View on GitHub</span>
+            <ExternalLinkIcon mx='2' />
+          </Link>
         </div>
-        <Link
-          className={styles['code-link']}
-          href='https://github.com/brianmanchester/carbon-footprint-calculator'
-          isExternal
-          color='teal.500'
-        >
-          <span>View on GitHub</span>
-          <ExternalLinkIcon mx='2' />
-        </Link>
       </div>
       {notifications.length > 0 && (
         <div className={styles.notifications}>
-          <Stack spacing={3}>
+          <Stack spacing='2'>
             {notifications.map(({ id, message, type }) => (
-              <Alert className={styles.alert} key={id} status={type}>
+              <Alert
+                className={styles.alert}
+                key={id}
+                status={type}
+                borderRadius='lg'
+              >
                 <div className={styles.alert}>
                   <AlertIcon />
                   <span>{message}</span>
