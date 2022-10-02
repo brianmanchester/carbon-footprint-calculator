1c156eab23244abeaaacc4f8fa42c44cf31f090f
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-01 18:49:48 -0600

    Remove logs

--- lib/db/supabase.ts
+++ lib/db/supabase.ts
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('API URL: ', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('API KEY: ', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 export const supabase = createClient(
   // @ts-ignore
   process.env.NEXT_PUBLIC_SUPABASE_URL,
