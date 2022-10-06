a6e8879884b0fe810e18411039a5a23c049aa9ae
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-05 22:09:32 -0600

    Add component to sum results

---
+++ components/total-footprint-sum/index.tsx
@@ -0,0 +1,34 @@
+import { Result } from '@/contexts/emissions-results/types';
+import { Box, Grid, GridItem, Text } from '@chakra-ui/react';
+
+export type TotalFootprintSumProps = {
+  marginBottom?: string;
+  results: Result[];
+};
+
+export const TotalFootprintSum = ({
+  marginBottom,
+  results
+}: TotalFootprintSumProps) => {
+  const total = results
+    .map(r => r.emissions)
+    .reduce((emissions, sum) => sum + emissions, 0);
+
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' gap='32px'>
+      <GridItem colStart={2}>
+        <Box
+          borderRadius='lg'
+          boxShadow='base'
+          marginBottom={marginBottom}
+          padding={4}
+          width='full'
+        >
+          <Text fontWeight='bold' fontSize='lg'>
+            Total per year: {total}
+          </Text>
+        </Box>
+      </GridItem>
+    </Grid>
+  );
+};
