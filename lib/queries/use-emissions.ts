28a3e47ab25d982733acc8e615d4b00864a24cd0
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-05 22:14:53 -0600

    Fix query by adding uses to key

--- lib/queries/use-emissions.ts
+++ lib/queries/use-emissions.ts
@@ -2,12 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { EmissionsPayload } from '../data/emissions';
 import { http } from '../http';
 
-export const useEmissions = (subCategoryId: number, use: number) =>
+export const useEmissions = (subCategoryId: number, use: string) =>
   useQuery<EmissionsPayload, Error>(
     ['emissions', { subCategoryId, use }],
     async () =>
       await http.get<EmissionsPayload>(
         `/api/sub-categories/${subCategoryId}/emissions`,
-        { use: String(use) }
-      )
+        { use }
+      ),
+    { enabled: !!use }
   );
