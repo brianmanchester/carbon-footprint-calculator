1a64c3f93a82ca34f9f0c27b09deb62af9c1f79d
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-06 20:18:11 -0600

    Fix calculation to make more accurate

--- lib/api/calculate-emissions/index.ts
+++ lib/api/calculate-emissions/index.ts
@@ -1,1 +1,16 @@
-export const calculateEmissions = (use: number, factor: number) => use * factor;
+const KG_TO_LBS = 2.205;
+
+export const calculateEmissions = (
+  use: number,
+  factor: number,
+  measurement: 'lbs' | 'kg'
+) => {
+  let result = use * factor;
+
+  // If kg, convert to lbs
+  if (measurement === 'kg') {
+    result = result * KG_TO_LBS;
+  }
+
+  return result;
+};
