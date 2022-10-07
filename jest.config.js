f206b234366de21c835e6d1eb60d432023879d63
Brian Manchester <brian.l.manchester@gmail.com>
2022-10-06 21:12:19 -0600

    Fix jest config to recognize ts path aliases

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,20 @@ const createJestConfig = nextJest({
   dir: './'
 });
 
-// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/contexts/(.*)$': '<rootDir>/contexts/$1',
+    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1'
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
